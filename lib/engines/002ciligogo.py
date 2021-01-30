81d8e4d263cb5318d78dd20e28a65111cee5126f
feelfar <feelfar@hotmail.com>
2021-01-30 22:16:58 +0800

    更换同模板网站-磁力蜘蛛btmovi.com

--- lib/engines/002ciligogo.py
+++ lib/engines/002ciligogo.py
@@ -9,7 +9,7 @@ from traceback import format_exc
 from xbmcswift2 import Plugin
 plugin=Plugin()
 class ciligogo(object):
-    url = 'http://www.ciligogo.info'
+    url = 'http://www.btmovi.buzz/so'
     name = 'ciligogo'
     support_sort=['relevance','addtime','size','popular'];
     page_result_count=10;
@@ -29,13 +29,13 @@ class ciligogo(object):
         elif sorttype=='relevance': sorttype='rel'
         else : sorttype='hits'
         
-        searchurl='%s/search/%s_%s_%s.html'%(self.url,parse.quote(what),str(sorttype),str(int(page)))
+        searchurl='%s/%s_%s_%s.html'%(self.url,parse.quote(what),str(sorttype),str(int(page)))
         #plugin.notify(searchurl)
         try:
             pageresult = _http(searchurl)
             
             #plugin.log.error(pageresult)
-            rmain=r'\x2Fmagnet\x2F(?P<magnet>[a-z0-9]{40})\x2Ehtml.*?blank[\x22\x27]\x3E(?P<title>.*?)\x3C\x2Fa\x3E.*?创建时间.*?\x3Cb\x3E(?P<createtime>.*?)\x3C\x2Fb\x3E.*?文件大小.*?\x3E(?P<filesize>.*?)\x3C\x2Fb\x3E'
+            rmain=r'\x2Fbt\x2F(?P<magnet>[a-z0-9]{40})\x2Ehtml.*?blank[\x22\x27]\x3E(?P<title>.*?)\x3C\x2Fa\x3E.*?创建时间.*?\x3Cb\x3E(?P<createtime>.*?)\x3C\x2Fb\x3E.*?文件大小.*?\x3E(?P<filesize>.*?)\x3C\x2Fb\x3E'
             reobj = re.compile(rmain, re.IGNORECASE | re.DOTALL)
             for match in reobj.finditer(pageresult):
                 
